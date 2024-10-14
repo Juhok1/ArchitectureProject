3f5e10aaba4f54b75f1b90e0b6e6f6b789206446
Juhok1 <juho21kauti@gmail.com>
2024-10-14 17:50:15 +0300

    Lähes valmis

--- ArchitectureProject.py
+++ ArchitectureProject.py
@@ -1,8 +1,18 @@
-
 import random
 import multiprocessing
-import time
+from multiprocessing import shared_memory
+import numpy as np
 
+def scheduler(share_memory_name, size):
+    existing_shm = shared_memory.SharedMemory(name=share_memory_name)
+
+    # Luetaan data
+    data = np.ndarray((size,), dtype=np.int64, buffer=existing_shm.buf)
+
+    # Tulostetaan luettavat arvot
+    print("Lajiteltu lista:", sorted(data))
+
+    existing_shm.close()
 
 luvut = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
@@ -21,14 +31,28 @@ def parentProcess():
         pipes.append(parent_pipe)
         child.start()
         
-        
+    # Luodaan yhteinen muisti
+    shm = shared_memory.SharedMemory(create=True, size=4 * np.int64().nbytes)
 
-    for child, parent_pipe in zip(childProcesses, pipes):
+    # Muuttuja, johon tallennetaan lapset
+    shm_array = np.ndarray((4,), dtype=np.int64, buffer=shm.buf)
+
+    for index, (child, parent_pipe) in enumerate(zip(childProcesses, pipes)):
         print(f"Käynnistetään lapsiprosessi {child.pid} ja odotetaan sitä")
         received_number = parent_pipe.recv()
         print(f"Lapsiprosessin valitsema luku on {received_number}")
+        
+        # Tallennetaan saatu luku yhteiseen muistiin
+        shm_array[index] = received_number
+
         child.join()
         print(f"Lapsiprosessi {child.pid} päättynyt")
+
+    # Käynnistetään scheduler
+    scheduler(shm.name, 4)
+
+    # Irrotetaan yhteinen muisti
+    shm.close()
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
